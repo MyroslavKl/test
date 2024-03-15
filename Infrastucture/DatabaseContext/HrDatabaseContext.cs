605b22b4719590394d9fcab30ca5409d8605edab
Myroslav <muroslavkl@gmail.com>
2024-03-15 18:05:05 +0200

    Add

---
+++ Infrastucture/DatabaseContext/HrDatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Pimpochki.Domain.Entities;
+
+namespace HR.LeaveManagement.Persistence.DatabaseContext
+{
+    public class HrDatabaseContext : DbContext
+    {
+
+        public HrDatabaseContext(DbContextOptions<HrDatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrDatabaseContext).Assembly);
+        //    base.OnModelCreating(modelBuilder);
+        //}
+        
+
+    }
+}
